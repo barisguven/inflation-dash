0ab605fd8c4ac150eb1f0edddd9c842ec0ed312b
barisguven <barisguven1@gmail.com>
2024-11-21 15:16:36 +0300

    add two adjacent card to contain plots

--- dash-shiny/app.R
+++ dash-shiny/app.R
@@ -17,12 +17,25 @@ ui <- page_sidebar(
     selectInput(
       "country",
       "Select a country:",
-      choices = c(unique(data$reference_area))
+      choices = c(unique(data$reference_area)),
+      selected = "Türkiye"
+    )
+  ),
+  layout_columns(
+    card(
+      card_header("Contributions to Annual Inflation in Percentage Points"),
+      plotOutput("decomp"),
+      #plotOutput("cpi_vs_def")
+    ),
+    card(
+      card_header("GDP Deflator vs. Consumer Price Index"),
+      plotOutput("def_vs_cpi")
     )
   ),
   card(
-    plotOutput("decomp"),
-    #plotOutput("cpi_vs_def")
+    card_header("Contributions of Unit Labor Cost, Unit Profit, and Unit Tax to the Percentage Change in GDP Deflator"),
+    tableOutput("table"),
+    height = "250px"
   )
 )
 
@@ -37,7 +50,7 @@ server <- function(input, output, session) {
       labs(x="", y="Percent") +
       geom_line(
         data = filter(data, reference_area == input$country, series == "inflation_def"), 
-        aes(time, value, color = "inflation_def")) +
+        aes(time, value, color = "inflation_def"), size=0.6) +
       scale_fill_viridis_d(
         breaks = c("contr_unit_labor_cost", "contr_unit_profit", "contr_unit_tax"),
         labels = c("Unit labor cost", "Unit profit", "Unit tax"),
@@ -47,9 +60,56 @@ server <- function(input, output, session) {
         values = c("inflation_def" = "red")
       ) +
       theme(
+        axis.text = element_text(size = 12),
+        axis.title.y = element_text(size=14),
+        legend.text = element_text(size = 12),
         legend.title = element_blank(),
         legend.position = "bottom"
       )
+  })
+
+  output$def_vs_cpi <- renderPlot({
+    data |>
+      filter(series %in% c("inflation_def", "inflation_cpi")) |>
+      filter(reference_area == input$country) |>
+      ggplot(aes(time, value, color = series)) +
+      geom_line(size=0.7) +
+      scale_color_brewer(
+        breaks = c("inflation_def", "inflation_cpi"),
+        labels = c("Deflator", "Consumer Price Index"),
+        palette = "Dark2") +
+      labs(y = "Percent", x = "") +
+      theme(
+        axis.text = element_text(size = 12),
+        axis.title.y = element_text(size=14),
+        legend.text = element_text(size = 12),
+        legend.title = element_blank(),
+        legend.position = "bottom"
+      )
+  })
+
+  output$table <- renderTable({
+    data |>
+  filter(reference_area == input$country) |>
+  filter(series %in% c("inflation_def", "inflation_cpi", income_comps)) |>
+  pivot_wider(names_from = series, values_from = value) |>
+  select(c(reference_area, time, contr_unit_labor_cost,
+           contr_unit_profit, contr_unit_tax,
+           inflation_def, inflation_cpi)) |>
+  rename(
+    Country = reference_area,
+    Period = time,
+    `Unit labor cost` = contr_unit_labor_cost,
+    `Unit profit` = contr_unit_profit,
+    `Unit tax` = contr_unit_tax,
+    Deflator = inflation_def,
+    CPI = inflation_cpi
+  ) |>
+  arrange(desc(Period)) |>
+  gt() |>
+  fmt_number(columns = 3:7, decimals = 2) |>
+  fmt_date(columns = Period, date_style = "year_quarter") |>
+  opt_stylize(style = 6, color = "gray")
   })
 }
 
